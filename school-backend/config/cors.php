6aec93d78a945315074e4b95d3c5b1060fc61e11
Mac <mac@Macs-MacBook-Pro.local>
2026-07-19 13:22:34 +0700

    Configure CORS for Vercel frontend

--- school-backend/config/cors.php
+++ school-backend/config/cors.php
@@ -21,7 +21,8 @@ return [
 
     'allowed_origins' => [
     'http://localhost:5173', 
-    'http://127.0.0.1:5173'
+    'http://127.0.0.1:5173',
+    'https://norea-school-management-system.vercel.app',
     ],
 
     // 'allowed_origins' => ['*'],
